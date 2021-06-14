ff5f129b53c960a8b97a830b262ad291b80f895f
Kelvin DeCosta <decostakelvin@gmail.com>
2021-06-14 19:52:48 +0300

    build: Add `gatsby-cloud` plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     FAST_DEV: true,
   },
   plugins: [
+    // Optimization
     "gatsby-plugin-preact",
     {
       resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
@@ -11,6 +12,21 @@ module.exports = {
         analyzerMode: "static",
         reportFilename: `${__dirname}/.cache/report.html`,
         openAnalyzer: false,
+      },
+    },
+
+    // Deployment
+    {
+      resolve: "gatsby-plugin-gatsby-cloud",
+      options: {
+        allPageHeaders: [
+          "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
+        ],
+        headers: {
+          "/*.html": ["Cache-Control: public, no-cache"],
+          "/*page-data.json": ["Cache-Control: public, no-cache"],
+          "/*app-data.json": ["Cache-Control: public, no-cache"],
+        },
       },
     },
   ],
